52f4a152b3e13a658637ca535208332ddbf6227f
Juan Junqueira <juan.marquesjunq@gmail.com>
2021-09-14 13:20:09 -0300

    mapa

--- src/components/ButtonMap/index.tsx
+++ src/components/ButtonMap/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { ButtonProps } from "../../interfaces/Button.interface";
-import { Imagem, ButtonStyle } from "./styles";
+// import React from "react";
+// import { ButtonProps } from "../../interfaces/Button.interface";
+// import { Imagem, ButtonStyle } from "./styles";
 
-export default function Button({title, image, onPress, ...rest}:ButtonProps){
-    return(
-        <ButtonStyle onPress={onPress} {...rest}>
-            <Imagem source={image} />
-        </ButtonStyle>
-    );
-}
+// export default function Button({title, image, onPress, ...rest}:ButtonProps){
+//     return(
+//         <ButtonStyle onPress={onPress} {...rest}>
+//             <Imagem source={image} />
+//         </ButtonStyle>
+//     );
+// }
