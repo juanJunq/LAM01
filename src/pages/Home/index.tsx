1b9ff2e8ad23c3453abe56463277f4b2a1df3ded
Juan Junqueira <juan.marquesjunq@gmail.com>
2021-09-14 23:28:19 -0300

    map view

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,16 +3,18 @@ import React, {
   useEffect
 } from "react";
 import { useNavigation } from "@react-navigation/core";
-import { 
-  Platform, 
+import {  
   Text, 
   View, 
-  StyleSheet 
+  StyleSheet,
+  Dimensions 
 } from 'react-native';
+import MapView, {Region, Marker} from "react-native-maps";
 import * as Location from 'expo-location';
 import {
   Container, ImageBg 
 } from "./styles";
+
 
 
 
@@ -24,6 +26,7 @@ export default function Home() {
 
   const [location, setLocation] = useState<null | Location.LocationObject>(null);
   const [errorMsg, setErrorMsg] = useState<null | string>(null);
+  const [region, setRegion] = useState<Region>();
 
   useEffect(() => {
     (async () => {
@@ -35,6 +38,12 @@ export default function Home() {
 
       let location = await Location.getCurrentPositionAsync({accuracy:1});
       setLocation(location);
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      })
     })();
   }, []);
   let text = 'Waiting..';
@@ -46,7 +55,10 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
+      {!region && <Text style={styles.paragraph}>{text}</Text>}
+      {region && <MapView style={styles.map} region={region}>
+          <Marker coordinate={region}></Marker>
+        </MapView>}
     </View>
   );
 }
@@ -61,5 +73,9 @@ const styles = StyleSheet.create({
   paragraph: {
     fontSize: 18,
     textAlign: 'center',
+  },
+  map:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
