83dc75cb38c8d97dfddce041fffea1d3cdded507
Juan Junqueira <juan.marquesjunq@gmail.com>
2021-09-16 00:20:32 -0300

    clone

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import {Home, Config} from "../pages";
 import colors from "../styles/colors";
-import { HeaderImage } from "../components/Header/styles";
+import { EvilIcons } from '@expo/vector-icons';
 
 
 const Drawer = createDrawerNavigator();
@@ -12,18 +12,27 @@ export default function App(){
         <Drawer.Navigator
             screenOptions={{
                 headerShown: true,
-                headerStyle: { backgroundColor: `${colors.orange}`},
+                headerStyle: { backgroundColor: colors.orange },
                 headerTintColor: "white",
-                headerTitle: "AllCourtMap"
+                headerTitle: "AllCourtMap",
+                // drawerStyle:
             }}
         >
             <Drawer.Screen 
-            name="Home" component={Home}
-            options={{drawerLabel:"Home"}}
+            name="Home" 
+            component={Home}
+            options={{drawerLabel:"Home",
+    
+        }}
         />
            <Drawer.Screen 
             name="Config" component={Config}
-            options={{drawerLabel:"Configurações"}}
+            options={{
+                drawerLabel:"Configurações",
+                drawerIcon: ()=> (
+                    <EvilIcons name="gear" size={24} color="black" />
+                )
+            }}
         />
         </Drawer.Navigator>
     );
